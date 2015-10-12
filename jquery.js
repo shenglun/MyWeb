5681723b44bb49c4eb523ac05ea3aa6cf6b26c1d
shenglun <airplaneisshengwang@gmail.com>
2015-10-12 11:22:33 +0800

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -6,6 +6,6 @@ $(document).ready(function(){
     $("#name").css("background-color","red");
   });
   $("#name").mouseout(function(){
-    $("#name").css("background-color","green");
+    $("#name").css("background-color","opacity:0.0");
   });
 });
